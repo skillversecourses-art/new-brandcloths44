137433983abb7a0beefcd68d02ec3dcae0c5c01d
SkillVerse Courses <skillverse.courses@gmail.com>
2026-05-27 21:01:19 +0530

    Configure GitHub Pages deployment (HashRouter + Vite Base)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { CartProvider } from './context/CartContext'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
